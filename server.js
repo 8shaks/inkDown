d8dad0dcab36756d7ccc29d514721c3915281f89
Shakiran Sathiyanathan <shakiran.sathiyanathan@gmail.com>
2020-05-29 01:00:37 -0400

    Refactored code

--- server.js
+++ server.js
@@ -91,14 +91,7 @@ app.post("/contact-me/email", (req, res) => {
 });
 
 //Server static assets if in production for front-end
-// if (process.env.NODE_ENV === "production") {
-  //set static folder
   app.use(express.static("client/public"));
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  // });
-// }
-
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
